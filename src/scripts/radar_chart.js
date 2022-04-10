f24589418693ec5d0499d3f6184e3e9caece4a2a
Tommy <virethy@gmail.com>
2022-04-10 10:45:45 -0400

    Ajout des points.

--- src/scripts/radar_chart.js
+++ src/scripts/radar_chart.js
@@ -1,20 +1,69 @@
-
+// Constants used for style.
 const TOTAL_HEADERS_COUNT = 2
+const POINT_RADIUS = 3
+const N_STEPS = 4
+const TITLE_VERTICAL_MARGIN = 15
+const SHAPE_STROKE_WIDTH = 2
+const AXE_STROKE_WIDTH = .5
+const CIRCLE_STROKE_WIDTH = .3
+const LABELS_MULTIPLIER_FACTOR = 1.05
 
-export function getScale() {
+// Constants used to move scales.
+const MAX_MULTPLIER_FACTOR = 1.05
+const MIN_MULTIPLIER_FACTOR = 0.8
 
+// Possible steps used for viz.
+const STEP_CHOICES = [.1, .2, .5, 1, 2, 5, 10, 20, 50, 100]
+
+export function setScale(stepChoice, minValue) {
+    var maxValue = minValue + N_STEPS * STEP_CHOICES[stepChoice]
+    return d3.scaleLinear()
+        .domain([minValue, maxValue])
+        .range([0, 1])
 }
 
-export function getSteps() {
+export function setMins(data, attribute) {
+    var values = []
+    data.forEach(serie => {
+        values.push(serie[attribute])
+    });
 
+    return MIN_MULTIPLIER_FACTOR * d3.min(values)
+}
+
+export function setSteps(data, attribute) {
+    var values = []
+    data.forEach(serie => {
+        values.push(serie[attribute])
+    });
+
+    var minValue = MIN_MULTIPLIER_FACTOR * d3.min(values)
+    var maxValue = MAX_MULTPLIER_FACTOR * d3.max(values)
+
+    var difference = maxValue - minValue
+    for (var i in [...Array(STEP_CHOICES.length).keys()]) {
+        if (difference <= STEP_CHOICES[i] * N_STEPS)
+            return i
+    }
 }
 
 export function drawSteps() {
 
 }
 
-export function drawPoints(data, element, xCenter, yCenter) {
+export function drawPoints(data, element, xCenter, yCenter, scales) {
+    var stats = getStats(data)
+    var points = getCoordinates(stats, scales, xCenter, yCenter)
 
+    element.append('g')
+        .attr('class', 'points')
+        .selectAll('circle')
+        .data(points)
+        .enter()
+        .append('circle')
+        .attr('cx', function(d) { return d.x })
+        .attr('cy', function(d) { return d.y })
+        .attr('r', POINT_RADIUS)
 }
 
 export function drawAxes(data, element, xCenter, yCenter) {
@@ -29,50 +78,109 @@ export function drawAxes(data, element, xCenter, yCenter) {
         .append('line')
         .attr('x1', xCenter)
         .attr('y1', yCenter)
-        .attr('x2', function(d, i) { return xCenter * (1 - Math.sin(i * 2 * Math.PI / totalAxes)); })
-        .attr('y2', function(d, i) { return yCenter * (1 - Math.cos(i * 2 * Math.PI / totalAxes)); })
+        .attr('x2', function(_, i) { return xCenter * (1 - Math.sin(i * 2 * Math.PI / totalAxes)); })
+        .attr('y2', function(_, i) { return yCenter * (1 - Math.cos(i * 2 * Math.PI / totalAxes)); })
         .attr('stroke', 'black')
-        .attr("stroke-width", "1px")
+        .attr('stroke-width', AXE_STROKE_WIDTH + 'px')
 }
 
 export function drawAxesLabel(data, element, xCenter, yCenter) {
-    // Remove unnecessary labels.
-    var stats = Object.assign({}, data)
-    delete stats.Team
-    delete stats.Season
+    var stats = getStats(data)
 
     // Build array containing labels.
     var labels = Object.keys(stats)
     var totalAxes = labels.length
 
     element.append('g')
-        .attr('class', 'axes')
+        .attr('class', 'labels')
         .selectAll('text')
         .data(labels)
         .enter()
         .append('text')
-        .attr('x', function(d, i) { return xCenter * (1 - Math.sin(i * 2 * Math.PI / totalAxes)); })
-        .attr('y', function(d, i) { return yCenter * (1 - Math.cos(i * 2 * Math.PI / totalAxes)); })
+        .attr('x', function(_, i) { return xCenter * (1 - LABELS_MULTIPLIER_FACTOR * Math.sin(i * 2 * Math.PI / totalAxes)); })
+        .attr('y', function(_, i) { return yCenter * (1 - LABELS_MULTIPLIER_FACTOR * Math.cos(i * 2 * Math.PI / totalAxes)); })
         .attr('text-anchor', 'middle')
         .text(function(d) { return d; })
 }
 
-export function drawCircles() {
+export function drawCircles(stepChoices, mins, scales, element, xCenter, yCenter) {
+    var nCircles = [...Array(N_STEPS).keys()]
+    var radiuses = []
 
+    var label = Object.keys(mins)[0]
+    var minValue = mins[label]
+
+    nCircles.forEach((i) => {
+        var value = minValue + i * STEP_CHOICES[stepChoices[label]]
+        console.log(stepChoices)
+        var radius = yCenter * (1 - scales[label](value))
+        console.log(radius)
+        radiuses.push(radius)
+    })
+
+    element.append('g')
+        .attr('class', 'circles')
+        .selectAll('circle')
+        .data(radiuses)
+        .enter()
+        .append('circle')
+        .attr('cx', xCenter)
+        .attr('cy', yCenter)
+        .attr('r', function(d) { return d; })
+        .attr('fill', 'transparent')
+        .attr('stroke-width', CIRCLE_STROKE_WIDTH + 'px')
+        .attr('stroke', 'black')
+        .style('stroke-dasharray', ('4, 2'))
 }
 
-export function drawShape() {
+export function drawShape(data, element, xCenter, yCenter, scales) {
+    var stats = getStats(data)
+    var points = getCoordinates(stats, scales, xCenter, yCenter)
 
+    var strPolygon = ''
+    points.forEach((point) => {
+        strPolygon += (point.x + ',' + point.y + ' ')
+    })
+
+    element.select('.points')
+        .append('polygon')
+        .attr('points', strPolygon)
+        .attr('fill', 'transparent')
+        .attr('stroke-width', SHAPE_STROKE_WIDTH + 'px')
+        .attr('stroke', 'black')
 }
 
 export function drawTitle(data, element, xCenter) {
     element.append('text')
         .text(data.Season)
         .attr('x', xCenter)
-        .attr('y', -10)
+        .attr('y', -TITLE_VERTICAL_MARGIN)
         .attr('text-anchor', 'middle')
         .style('color', 'black')
         .style('font-size', '12px')
+}
+
+export function getStats(data) {
+    // Removes unnecessary labels.
+    var stats = Object.assign({}, data)
+    delete stats.Team
+    delete stats.Season
+
+    return stats
+}
+
+export function getCoordinates(stats, scales, xCenter, yCenter) {
+    var points = []
+    var keys = Object.keys(stats)
+    var totalAxes = keys.length
+
+    keys.forEach(function(d, i) {
+        var x = xCenter * (1 - scales[d](stats[d]) * Math.sin(i * 2 * Math.PI / totalAxes))
+        var y = yCenter * (1 - scales[d](stats[d]) * Math.cos(i * 2 * Math.PI / totalAxes))
+        points.push({'x': x, 'y': y})
+    })
+
+    return points
 }
 
 export function eraseAll(element) {
--- src/scripts/viz.js
+++ src/scripts/viz.js
@@ -4,8 +4,11 @@ import * as radarChart from './radar_chart'
  * Calls the functions from radar_chart.js to build the radar
  * chart of the offensive stats.
  *
- * @param {*} data The data used to build the radar chart.
- * @param {*} element The SVG element where the radar chart will be built.
+ * @param { Array } data The data used to build the radar chart.
+ * @param { SVGElement } element The SVG element where the radar chart will be built.
+ * @param { Number } width The width of the SVG tag.
+ * @param { Number } height The height of the SVG tag.
+ * @param { Object } margin The margins between the container and the SVG space.
  */
 export function drawOffensiveRadarCharts(data, element, width, height, margin) {
     var xCenter = width / 2
@@ -13,6 +16,22 @@ export function drawOffensiveRadarCharts(data, element, width, height, margin) {
 
     // Erases charts so they can be rebuilt.
     radarChart.eraseAll(element)
+
+    // Prepares scales, steps and mins.
+    var scales = {}
+    var steps = {}
+    var mins = {}
+    var stats = radarChart.getStats(data[0])
+    var labels = Object.keys(stats)
+
+    labels.forEach(function(label) {
+        var min = radarChart.setMins(data, label)
+        mins[label] = min
+        var stepChoice = radarChart.setSteps(data, label, min)
+        steps[label] = stepChoice
+        var scale = radarChart.setScale(stepChoice, min)
+        scales[label] = scale
+    })
 
     // Build container for charts.
     var container = element.append('g')
@@ -25,9 +44,13 @@ export function drawOffensiveRadarCharts(data, element, width, height, margin) {
             var chartContainer = container.append('g')
                 .attr('transform', 'translate(' + xTranslation + ' 0)')
 
+
             radarChart.drawAxes(serie, chartContainer, xCenter, yCenter)
             radarChart.drawAxesLabel(serie, chartContainer, xCenter, yCenter)
+            radarChart.drawCircles(steps, mins, scales, chartContainer, xCenter, yCenter)
             radarChart.drawTitle(serie, chartContainer, xCenter)
+            radarChart.drawPoints(serie, chartContainer, xCenter, yCenter, scales)
+            radarChart.drawShape(serie, chartContainer, xCenter, yCenter, scales)
         }
     });
     
